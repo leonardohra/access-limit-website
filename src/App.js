ef5f711bd45e5e07ca436cd6fc1c28c309ca7d61
Leo <leonardohra@gmail.com>
2023-05-09 10:33:34 -0300

    changed the picture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import cat from './cat-pic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cat} className="App-logo" alt="cat" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
